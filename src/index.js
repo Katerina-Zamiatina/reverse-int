e2187f77c716165661cc18219afa31d322740283
Katerina Zamiatina <eozamyatina@gmail.com>
2022-08-09 20:04:20 +0200

    feat: passed all tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const arr = ("" + Math.abs(n)).split("");
+    const revArr = arr.reverse();
+    console.log(revArr);
+    return Number(revArr.join(""));
+};
